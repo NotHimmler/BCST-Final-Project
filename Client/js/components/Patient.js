6b2e3cf5cf5fbde60dc34f56266a90aea9f306e0
Heinrich Malan <heinrich@heinbio.me>
2018-08-26 16:33:24 +1000

    Added server route to give correct auth url to patient view, depending on dev or build environments

--- Client/js/components/Patient.js
+++ Client/js/components/Patient.js
@@ -2,12 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class Patient extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {url: "#"};
+        fetch("/getAuthUrl").then(res => res.json())
+        .then(res => {
+            this.setState({url : res.url});
+        })
+    }
+
     render() {
         return (
             <div className="row w-100 d-flex justify-content-center text-center">
             <h1 className="">Patient</h1>
             <div className="row d-flex w-100 h-100 flex-column justify-content-center align-items-center">
-                <div className="d-flex flex-column"><a href="https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=22CYWW&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fphysio-dashboard&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">
+                <div className="d-flex flex-column"><a href={this.state.url}>
                     <img className="fitbit-logo" src="https://botw-pd.s3.amazonaws.com/styles/logo-original-577x577/s3/112015/fitbit_logo.png" />
                     <br />
                     Login with fitbit
--- Server/app/Routes.js
+++ Server/app/Routes.js
@@ -1,11 +1,19 @@
 var path = require('path');
 var request = require('request');
 
+const data = process.argv[2] == "dev" ? require('./TestConfig') : require('./BuildConfig');
+
 let code = "";
 let token = "";
 let refreshToken = "";
 
 module.exports = function(app) {
+app.get("/getAuthUrl", (req, res, err) => {
+    let url = "https://www.fitbit.com/oauth2/authorize?response_type=code&client_id="+data.client_id+"&redirect_uri="+data.redirect_uri+"&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800";
+    let toSend = {"url": url};
+    res.json(toSend);
+})
+
 app.get("/getSteps", (req, res, err) => {
     let options = {
         method: 'GET',
@@ -28,9 +36,9 @@ app.get("/physio-dashboard", (req, res, err) => {
         code = req.query.code;
         let options = {
             method: 'POST',
-            url : "https://api.fitbit.com/oauth2/token?clientId=22CYWW&grant_type=authorization_code&redirect_uri=http://localhost:8080/physio-dashboard&code="+code,
+            url : "https://api.fitbit.com/oauth2/token?clientId="+ data.client_id +"&grant_type=authorization_code&redirect_uri="+data.redirect_uri+"&code="+code,
             headers: {
-                'Authorization': 'Basic MjJDWVdXOjM5YzVkMmQwYWU2NDAwMjQ5MjgyOGIyYzczMDgyZmYy',
+                'Authorization': 'Basic ' + data.auth_basic,
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
         }
