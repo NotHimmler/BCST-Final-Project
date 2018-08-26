194189ca8fccfa3c70129db0051344eb7e0daeab
Heinrich Malan <heinrich@heinbio.me>
2018-08-26 15:42:10 +1000

    Changed header text from Health to Physio Dashboard

--- Client/js/components/Home.js
+++ Client/js/components/Home.js
@@ -6,7 +6,7 @@ class Home extends React.Component {
         return (
             <div className="row d-flex w-100 home-row">
                 <div className="row d-flex flex-column text-center bg-dark w-100">
-                    <h1 className="text-white d-block">Health Dashboard</h1>
+                    <h1 className="text-white d-block">Physio Dashboard</h1>
                     <br />
                     <p className="text-white">Hello, everyone! This is the home page of the dashboard!</p> 
                  </div>
