7899725303a5b9f2fd19fbd44425e08b7a7609b7
Heinrich Malan <heinrich@Heinrichs-MacBook-Pro.local>
2018-08-26 11:32:33 +1000

    Changed home banner to remove rounded corners and added margin to row belpw

--- Client/js/components/Home.js
+++ Client/js/components/Home.js
@@ -5,17 +5,17 @@ class Home extends React.Component {
     render() {
         return (
             <div className="row d-flex w-100 home-row">
-                <div className="row d-flex flex-column jumbotron text-center bg-dark w-100">
+                <div className="row d-flex flex-column text-center bg-dark w-100">
                     <h1 className="text-white d-block">Health Dashboard</h1>
                     <br />
                     <p className="text-white">Hello, everyone! This is the home page of the dashboard!</p> 
                  </div>
           
-                <div className="row w-100">
+                <div className="row w-100 mt-1">
                     <div className="col-md-6">
                         <Link to="/doctorLogin">
                         <img href="#" src = "/img/doctor.jpg" className = "rounded-circle img-circle" />
-                        <p className="text-center word">Doctor</p>
+                        <p className="text-center word">Physiotherapist</p>
                         </Link>
                     </div>
                     <div className="col-md-6">
