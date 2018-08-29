3790f989ee95daa829e561d495db4e9f8ca5065f
Heinrich Malan <heinrich@heinbio.me>
2018-08-29 20:10:15 +1000

    Added unit test for DoctorLogin Component

---
+++ Client/js/components/DoctorLogin.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { mount } from "enzyme";
+import DoctorLogin from "./DoctorLogin";
+
+const mockTrySetLoggedIn = jest.fn();
+
+class MockSessionStore {
+    setLoggedIn(val) {
+        mockTrySetLoggedIn();
+    } 
+}
+
+let mockSessionStore = new MockSessionStore();
+
+describe("DoctorLogin", () => {
+    let mountedDoctorLogin;
+    const doctorLogin = () => {
+        if(!mountedDoctorLogin) {
+            mountedDoctorLogin = mount(<DoctorLogin sessionStore={mockSessionStore} history={[]}/>);
+        }
+    
+        return mountedDoctorLogin;
+    }
+    
+    beforeEach(() => {
+        mountedDoctorLogin = undefined;
+    });
+
+    it("always renders a div", () => {
+        const divs = doctorLogin().find("div");
+        expect(divs.length).toBeGreaterThan(0);
+    });
+
+    it("always renders a form", () => {
+        const form = doctorLogin().find("form");
+        expect(form.length).toBeGreaterThan(0);
+    });
+
+    it("should try to set login store to logged in", () => {
+        const component = doctorLogin();
+        
+        component.find('form').first().simulate('submit');
+        expect(mockTrySetLoggedIn).toHaveBeenCalled();
+    });
+
+    describe("the rendered div", () => {
+        it("contains everything else that gets rendered", () => {
+            const divs = doctorLogin().find("div");
+            const wrappingDiv = divs.first();
+    
+            expect(wrappingDiv.children()).toEqual(doctorLogin().find("div").first().children());
+        });
+      });
+});
