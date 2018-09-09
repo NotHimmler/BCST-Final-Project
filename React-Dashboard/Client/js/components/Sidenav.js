e72e39efd8053dfaedbe7dd757ae0e441d530fd0
jinalee96 <jina.a.lee96@gmail.com>
2018-09-09 21:39:55 +1000

    sidenav branch

---
+++ React-Dashboard/Client/js/components/Sidenav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Sidenav extends React.Component {
+
+    render() {
+        return
+    }
+
+}
+
+export default Sidenav;
