b069f899be3c711dcaaf4dd373d58aa5af4916b9
jinalee96 <jina.a.lee96@gmail.com>
2018-09-11 18:29:07 +1000

    login button readded

--- React-Dashboard/Client/js/components/Nav.js
+++ React-Dashboard/Client/js/components/Nav.js
@@ -16,6 +16,7 @@ class Nav extends React.Component {
         return (
             <nav id="topnav" className="navbar navbar-expand navbar-light">
                 <a id="logo" href="#">PHYSIODASHBOARD</a>
+                { !this.props.sessionStore.loggedIn ? <span className="nav-item nav-link"><Link className="text-white" to="/login">Login</Link></span> : <span className="nav-item nav-link"><Link className="text-white" onClick={this.handleLogout} to="/">Logout</Link></span>}
             </nav>
         )
     }
