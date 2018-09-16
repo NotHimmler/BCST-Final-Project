6d8f53e2c1eb51404f3b5acbdd83efa99cb0af5d
jinalee96 <jina.a.lee96@gmail.com>
2018-09-16 11:20:28 +1000

    Created new foler for merged dashboard

---
+++ Merged-Dashboard/Client/js/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to the dashboard!</p>
+                <p>This is the home page.</p>
+            </div>
+        )
+    }
+}
+
+export default Home;
