d8256ae0d625b1eb56f902849c1174f357fc9b51
jinalee96 <jina.a.lee96@gmail.com>
2018-09-16 12:43:10 +1000

    import vendors

---
+++ Merged-Dashboard/Client/js/components/Test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Test extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to the dashboard!</p>
+                <p>This is the home page.</p>
+            </div>
+        )
+    }
+}
+
+export default Test;
