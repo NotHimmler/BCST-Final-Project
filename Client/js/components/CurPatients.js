9e09ef53ef78d9cc793eed041730c2a8ef500ee6
jinalee96 <jina.a.lee96@gmail.com>
2018-09-20 01:17:25 +1000

    browser router added and works with sidebar

---
+++ Client/js/components/CurPatients.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class CurPatients extends React.Component {
+    
+    render() {
+        return (
+            <div>
+                <p>These are the current patients</p>
+            </div>
+        )
+    }
+}
+
+export default CurPatients;
