c209b06c23769120c4dcde7ecd024717e7b992ea
jinalee96 <jina.a.lee96@gmail.com>
2018-09-20 01:01:17 +1000

    disabled sidebar toggle

--- Client/js/components/Sidebar.js
+++ Client/js/components/Sidebar.js
@@ -7,6 +7,36 @@ import { Link } from "react-router-dom";
 
 class Sidebar extends React.Component {
     componentDidMount() {
+
+      (function($,sr){
+        // debouncing function from John Hann
+        // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+        var debounce = function (func, threshold, execAsap) {
+          var timeout;
+    
+            return function debounced () {
+                var obj = this, args = arguments;
+                function delayed () {
+                    if (!execAsap)
+                        func.apply(obj, args);
+                    timeout = null;
+                }
+    
+                if (timeout)
+                    clearTimeout(timeout);
+                else if (execAsap)
+                    func.apply(obj, args);
+    
+                timeout = setTimeout(delayed, threshold || 100);
+            };
+        };
+    
+        // smartresize
+        jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+    
+    })(jQuery,'smartresize');
+
+
       var CURRENT_URL = window.location.href.split('#')[0].split('?')[0],
           $BODY = $('body'),
           $MENU_TOGGLE = $('#menu_toggle'),
@@ -65,6 +95,7 @@ class Sidebar extends React.Component {
       });
   
   // toggle small or large menu
+  /*
   $MENU_TOGGLE.on('click', function() {
       console.log('clicked - menu toggle');
   
@@ -81,7 +112,7 @@ class Sidebar extends React.Component {
     setContentHeight();
   
     $('.dataTable').each ( function () { $(this).dataTable().fnDraw(); });
-  });
+  });*/
   
     // check active menu
     $SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
