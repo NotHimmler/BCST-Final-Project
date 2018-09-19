658b9fb45a9f755a0da2f9ffe1c5e37c68eb0627
jinalee96 <jina.a.lee96@gmail.com>
2018-09-20 00:35:11 +1000

    testing jquery

--- Client/js/components/Test.js
+++ Client/js/components/Test.js
@@ -2,11 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class Test extends React.Component {
+
+    componentDidMount(){
+
+        $("button").click(function(){
+            alert("HI!");
+              });
+
+    }
+    
     render() {
         return (
             <div>
                 <p>Welcome to the dashboard!</p>
                 <p>This is the home page.</p>
+                <button>Hi!</button>
             </div>
         )
     }
