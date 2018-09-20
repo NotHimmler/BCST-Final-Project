7d4e2d3c983413d4a26d6e3ccdb23a3683be3c30
jinalee96 <jina.a.lee96@gmail.com>
2018-09-21 00:30:56 +1000

    sidebar active page fixed

--- Merged-Dashboard/Client/js/components/PatientSidebar.js
+++ Merged-Dashboard/Client/js/components/PatientSidebar.js
@@ -10,9 +10,12 @@ class PatientSidebar extends React.Component {
           <div id="sidebar-menu" className="main_menu_side hidden-print main_menu">
           <div className="menu_section">
             <ul className="nav side-menu">
-            <li><a href="/patient"><i class="fa fa-bar-chart-o"></i> Patient Data </a>
+            <li>
+              <Link to="patient">
+              <i class="fa fa-bar-chart-o"></i> Patient Data 
+              </Link>
               </li>
-              <li><a href="/patient/goal"><i class="fa fa-table"></i> Goals </a>
+              <li><Link to="patient-goal"><i class="fa fa-table"></i> Goals </Link>
                   </li>
 
             </ul>
