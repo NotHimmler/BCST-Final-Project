fa2b51de31e7c3140dfa1dadcb3bed9f0fb0b5e9
Gerhard Malan <gmal6100@uni.sydney.edu.au>
2018-10-01 16:31:55 +1000

    Changed gitignore to ignore dsstore

---
+++ WalkforwardTest/WalkforwardTest/CurrentWalkViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CurrentWalkViewController.swift
+//  WalkAroundTheBlock
+//
+//  Created by Heinrich Malan on 1/10/18.
+//  Copyright © 2018 Heinrich Malan. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWalkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
