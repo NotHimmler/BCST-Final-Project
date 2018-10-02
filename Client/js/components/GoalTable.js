5ac2d5cf3793e7c203b58dcd08815d29fac96d0d
jinalee96 <jina.a.lee96@gmail.com>
2018-10-02 21:12:35 +1000

    Christina: goals red if completed

--- Client/js/components/GoalTable.js
+++ Client/js/components/GoalTable.js
@@ -22,10 +22,10 @@ class GoalTable extends React.Component {
                       <button data-toggle="dropdown" className="btn btn-success dropdown-toggle btn-xs" type="button">Goals <span className="caret"></span>
                       </button>
                       <ul role="menu" className="dropdown-menu" id="1_week">
-                        <li><a href="#">Side walking</a></li>
-                        <li><a href="#">Keep lifting</a></li>
-                        <li><a href="#">Sit to stand</a></li>
-                        <li><a href="#">Dynamic balance</a></li>
+                        <li><a id="red" href="#">Side walking</a></li>
+                        <li><a id="red" href="#">Keep lifting</a></li>
+                        <li><a id="red" href="#">Sit to stand</a></li>
+                        <li><a id="red" href="#">Dynamic balance</a></li>
                         <li><a href="#">Walk for 500 steps</a></li>
                       </ul>
                     </div>
@@ -39,10 +39,10 @@ class GoalTable extends React.Component {
                       <button data-toggle="dropdown" className="btn btn-success dropdown-toggle btn-xs" type="button">Goals <span className="caret"></span>
                       </button>
                       <ul role="menu" className="dropdown-menu" id="2_week">
-                        <li><a href="#">Side walking</a></li>
-                        <li><a href="#">Keep lifting</a></li>
-                        <li><a href="#">Sit to stand</a></li>
-                        <li><a href="#">Dynamic balance</a></li>
+                        <li><a id="red" href="#">Side walking</a></li>
+                        <li><a id="red" href="#">Keep lifting</a></li>
+                        <li><a id="red" href="#">Sit to stand</a></li>
+                        <li><a id="red" href="#">Dynamic balance</a></li>
                         <li><a href="#">Walk for 500 steps</a></li>
                       </ul>
                     </div>
@@ -56,15 +56,15 @@ class GoalTable extends React.Component {
                       <button data-toggle="dropdown" className="btn btn-success dropdown-toggle btn-xs" type="button">Goals <span className="caret"></span>
                       </button>
                       <ul role="menu" className="dropdown-menu" id="3_week">
-                        <li><a href="#">Side walking</a></li>
-                        <li><a href="#">Keep lifting</a></li>
+                        <li><a id="red" href="#">Side walking</a></li>
+                        <li><a id="red" href="#">Keep lifting</a></li>
                         <li><a href="#">Sit to stand</a></li>
                         <li><a href="#">Dynamic balance</a></li>
                         <li><a href="#">Walk for 500 steps</a></li>
                       </ul>
                     </div>
                   </td>
-                  <td id="situation_3">0/5</td>
+                  <td id="situation_3">2/5</td>
                 </tr>
                 <tr>
                   <th scope="row">10.8-10.14</th>
