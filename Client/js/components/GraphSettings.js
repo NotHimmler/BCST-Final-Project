55f7504a21ca3c5fffb658b283244e197935af70
jinalee96 <jina.a.lee96@gmail.com>
2018-10-04 23:40:36 +1000

    put graph settings in x_panel

--- Client/js/components/GraphSettings.js
+++ Client/js/components/GraphSettings.js
@@ -7,30 +7,40 @@ class GraphSettings extends React.Component {
         return (
             <div>
               {/*ALL HTML MUST BE WITHIN THIS DIV*/}
-              <p>Goal Settings here</p>
-                <div class="form-group">
-                    <label class="col-md-3 col-sm-3 col-xs-12 control-label">Select the data you want to view:
-                    </label>
+              <div className="x_panel">
+                <div className="x_title">
+                    <h2>Data Settings</h2>
+                    <div className="clearfix"></div>            
+                </div>
+                <div className="x_content">
+                    <div className="form-group">
+                        <label className="col-md-3 col-sm-3 col-xs-12 control-label">Select the data you want to view:
+                        </label>
 
-                    <div class="col-md-9 col-sm-9 col-xs-12">
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" value=""/> Fitbit Application data.
-                            </label>
+                        <div className="col-md-9 col-sm-9 col-xs-12">
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" value=""/> Fitbit Application data.
+                                </label>
+                            </div>
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" value=""/> AMOUNT Application data.
+                                </label>
+                            </div>
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" value=""/> WALKFORWARD Application data.
+                                </label>
+                            </div>
+                            <button type="submit" className="btn btn-success">Submit</button>
                         </div>
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" value=""/> AMOUNT Application data.
-                            </label>
-                        </div>
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" value=""/> WALKFORWARD Application data.
-                            </label>
-                        </div>
-                        <button type="submit" class="btn btn-success">Submit</button>
                     </div>
                 </div>
+
+              </div>
+
+                
             </div>
 
         )
