b4bd4efdaa2ff2cb3a1791d1f5e13db524216b3e
Heinrich Malan <heinrich@heinbio.me>
2018-10-12 15:24:08 +1100

    Added models for patient, app data

--- Merged-Dashboard/Server/config.js
+++ Merged-Dashboard/Server/config.js
@@ -26,6 +26,53 @@ let modelsConfig = [{
             }
         }
     },
+    {
+        modelName: "Patient_Info",
+        column: {
+            "MRN": {
+                type: Sequelize.STRING
+            },
+            "admin": {
+                type: Sequelize.BOOLEAN,
+                defaultValue:false
+            },
+            "password": {
+                type: Sequelize.STRING
+            },
+            "firstName": {
+                type: Sequelize.STRING
+            },
+            "lastName": {
+                type: Sequelize.STRING
+            }
+        }
+    },
+    {
+        modelName: "App_Report",
+        column: {
+            "MRN": {
+                type: Sequelize.STRING
+            },
+            "date": {
+                type: Sequelize.DATE
+            },
+            "numSteps": {
+                type: Sequelize.INTEGER
+            },
+            "distance": {
+                type: Sequelize.INTEGER
+            },
+            "duration": {
+                type: Sequelize.INTEGER
+            },
+            "goalType": {
+                type: Sequelize.STRING
+            },
+            "goalValue": {
+                type: Sequelize.INTEGER
+            }
+        }
+    },
     // add models config here
     // {
     //  modelName: "Test_Model",
