88e7116b287b56fce6c260ffa800c5f6b97ec722
jinalee96 <jina.a.lee96@gmail.com>
2018-10-18 02:26:46 +1100

    created new database and initialised new models/migrations

---
+++ Database/models/user.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    user_id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+    User.hasMany(models.Goal, {
+      foreignKey: 'created_by',
+      onDelete: 'CASCADE'
+    });
+  };
+  return User;
+};
