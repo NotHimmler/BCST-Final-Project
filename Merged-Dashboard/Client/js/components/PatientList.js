db35a99668f0612feec5db7be1547589d5e5f639
Qiang Qu <qiqu7152@uni.sydney.edu.au>
2018-10-18 20:39:51 +1100

    patient list ajax

--- Merged-Dashboard/Client/js/components/PatientList.js
+++ Merged-Dashboard/Client/js/components/PatientList.js
@@ -150,6 +150,26 @@ class PatientList extends React.Component {
   }
 
   componentDidMount() {
+    let therapistId = 0;
+    $.ajax({
+        // url:'/api/v1/therapist/patientList?therapistId=${this.props.username}',
+        url:'/api/v1/therapist/patientList?therapistId=${therapistId}',
+        type:"get",
+        contentType:"application/json;charset=utf-8",
+        success: (data)=>{
+            console.log(data.patientIds);
+            console.log("pppppppappppkfoa");
+            let error = data.error;
+            if (error) {
+              this.setState({
+                errorMessage:error
+              });
+            } else {
+                // patients = data;
+            }
+        }
+    });
+
     this.setState({"archived": this.props.archived});
 
     let rows = []
