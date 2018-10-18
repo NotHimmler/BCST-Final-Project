e3f62355a343b9640f9338e8e6c1d611f5de24bb
Gerhard Malan <gmal6100@uni.sydney.edu.au>
2018-10-18 16:53:35 +1100

    Added api call to get data for an mrn and tests for the component

--- Merged-Dashboard/Client/js/components/WalkAppTable.js
+++ Merged-Dashboard/Client/js/components/WalkAppTable.js
@@ -1,15 +1,57 @@
 import React from "react";
 import { Link } from "react-router-dom";
+let $ = require("jquery");
 
 
+let generateWalkTableRow = (data) => {
+    return (
+        <tr key={data.date}>
+            <td>{(new Date(data.date*1000)).toDateString()}</td>
+            <td>{data.numSteps}</td>
+            <td>{data.distance}</td>
+            <td>{data.duration}</td>
+            <td>{data.goalType}</td>
+            <td>{data.goalValue}</td>
+        </tr>
+    )
+}
 
 class WalkAppTable extends React.Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props);
+    this.state = {
+        rows: []
+    }
   }
 
   componentDidMount() {
+      
+      this.getWalkData()
+    }
+
+    getWalkData() {
+        if (this.props.getWalkData) this.props.getWalkData();
+        let mrn = this.props.mrn;
+        let data = JSON.stringify({"mrn": mrn});
+        $.ajax({
+            url: "/api/v1/getWalkData",
+            type: "post",
+            data: data,
+            contentType:"application/json;charset=utf-8",
+            success: (data) => {
+                console.log(data)
+                let rows = []
+                for (let item in data){
+                    rows.push(generateWalkTableRow(data[item]))
+                }
+                this.setState({rows: rows})
+                console.log("Fetch succeeded")
+            }, 
+            error: data => {
+                console.log("Fetch failed")
+            }
+        })
     }
     
     render() {
@@ -18,12 +60,15 @@ class WalkAppTable extends React.Component {
           <div>
               <table className="table">
                 <thead>
+                    <tr>
                     <th>Date</th>
                     <th>Num Steps</th>
                     <th>Distance (meters)</th>
                     <th>Duration (minutes)</th>
                     <th>Goal</th>
                     <th>Goal Type</th>
+                    </tr>
+                    {this.state.rows}
                 </thead>
               </table>
           </div>
