347808ba7fb34d2c6d1b821ee0a7843f47acdae6
jinalee96 <jina.a.lee96@gmail.com>
2018-10-20 21:46:02 +1100

    made fitbitdata routes for graph

--- Merged-Dashboard/Server/app/FitbitRouter.js
+++ Merged-Dashboard/Server/app/FitbitRouter.js
@@ -3,10 +3,68 @@ var fitbitRouter = express.Router();
 const db = require('../../Database/models/index.js');
 db.FitbitTokens.sync();
 db.FitbitData.sync();
+
+const Sequelize = require('sequelize');
+const {gt, lte, in: opIn} = Sequelize.Op;
+
+// Get all fb data
 fitbitRouter.get('/', function(req,res) {
-    res.send("Hello World! This is fitbit router!");
+    return db.FitbitData.findAll()
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.log('There was an error querying contacts', JSON.stringify(err))
+      return res.send(err)
+    });
 });
 
+// Get fb data for patient with specific mrn
+fitbitRouter.get("/mrn/:mrn", function(req, res) {
+    //console.log(req.params.mrn)
+    return db.FitbitData.findAll({
+        where: {
+            MRN: req.params.mrn,
+        },
+        order: [['date', 'DESC']],
+    })
+    .then((data) => {
+        if (data == null) {
+            res.send({error: "No patient with this mrn"})
+        } else {
+            res.send(data)
+        }
+    })
+    .catch((err) => {
+        console.log('There was an error querying contacts', JSON.stringify(err))
+      return res.send(err)
+    });
+ });
+
+ // Get fb data for patient with specific mrn
+fitbitRouter.get("/mrn/:mrn/weekly", function(req, res) {
+    //console.log(req.params.mrn)
+    return db.FitbitData.findAll({
+        attributes: ["MRN", [Sequelize.fn('sum', Sequelize.col('steps')), 'steptotal']],
+        where: {
+            MRN: req.params.mrn,
+        },
+        order: [['date', 'DESC']],
+        group: ['MRN'],
+    })
+    .then((data) => {
+        if (data == null) {
+            res.send({error: "No patient with this mrn"})
+        } else {
+            res.send(data)
+        }
+    })
+    .catch((err) => {
+        console.log('There was an error querying contacts', JSON.stringify(err))
+      return res.send(err)
+    });
+ });
+
+// FITBIT AUTH STUFF
+//=============================================================================================
 fitbitRouter.get('/getAuthBasic', (req, res) => {
     const fitbitSecret = process.env.FBCS;
     const fitbitClient = process.env.FBCID;
