49c407b35aec64d2050d90d9ba9b8db3fa61febf
Gerhard Malan <gmal6100@uni.sydney.edu.au>
2018-10-21 16:18:08 +1100

    Added router for amount data

---
+++ Merged-Dashboard/Server/app/AmountRouter.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var amountRouter = express.Router();
+const db = require('../../Database/models/index.js');
+db.AmountData.sync();
+
+const Sequelize = require('sequelize');
+const {gt, lte, ne, like, in: opIn} = Sequelize.Op;
+
+// Get all fb data
+amountRouter.get('/', function(req,res) {
+    return db.AmountData.findAll()
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.log('There was an error querying contacts', JSON.stringify(err))
+      return res.send(err)
+    });
+});
+
+// Get fb data for patient with specific mrn
+amountRouter.get("/mrn/:mrn", function(req, res) {
+    //console.log(req.params.mrn)
+    return db.AmountData.findAll({
+        where: {
+            MRN: req.params.mrn,
+        },
+        order: [['date', 'DESC']],
+    })
+    .then((data) => {
+        if (data == null) {
+            res.send({error: "No patient with this mrn"})
+        } else {
+            res.send(data)
+        }
+    })
+    .catch((err) => {
+        console.log('There was an error querying contacts', JSON.stringify(err))
+      return res.send(err)
+    });
+ });
+
+// 404 not found
+amountRouter.get('*', function(req,res) {
+    res.status(404).send('404 not found')
+});
+
+module.exports = amountRouter;
