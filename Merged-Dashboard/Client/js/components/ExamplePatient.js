e5cb47934bdca54f4611f4ce74b6536f9aeb3808
Qiang Qu <qiqu7152@uni.sydney.edu.au>
2018-10-21 16:04:40 +1100

    fix small bug

--- Merged-Dashboard/Client/js/components/ExamplePatient.js
+++ Merged-Dashboard/Client/js/components/ExamplePatient.js
@@ -169,7 +169,7 @@ class ExamplePatient extends React.Component {
 
               {
                 (this.state.content === "Settings")
-                    ? <PatientSettings mrn={this.props.match.params.MRN} archived={data.is_archived}/>
+                    ? <PatientSettings mrn={this.props.match.rams.MRN} archived={data.is_archived}/>
                     : null
               }
 
