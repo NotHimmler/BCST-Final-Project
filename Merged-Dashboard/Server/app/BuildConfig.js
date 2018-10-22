2cc9780e39a377c4f56ce8cab845eef4f3a7530f
Gerhard Malan <gmal6100@uni.sydney.edu.au>
2018-10-22 14:33:45 +1100

    fixed bug misssing quote

--- Merged-Dashboard/Server/app/BuildConfig.js
+++ Merged-Dashboard/Server/app/BuildConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
     client_id: "22CYWW",
     auth_basic: "MjJDWVdXOjM5YzVkMmQwYWU2NDAwMjQ5MjgyOGIyYzczMDgyZmYy",
-    redirect_uri: "http://soft3413-physio-dashboard.herokuapp.com
+    redirect_uri: "http://soft3413-physio-dashboard.herokuapp.com"
 }
