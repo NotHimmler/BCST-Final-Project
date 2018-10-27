7e01b9b52c1d7e81adf32e5c7da56692b0c4cadd
paras gathani <pgat6537@uni.sydney.edu.au>
2018-10-27 19:26:51 +1100

    added unit test for GoalModal

---
+++ Merged-Dashboard/Client/js/components/GoalModal.test.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { mount } from "enzyme";
+import GoalModal from "./GoalModal";
+/** 
+const mockTrySetLoggedIn = jest.fn();
+
+class MockSessionStore {
+    setLoggedIn(val) {
+        mockTrySetLoggedIn();
+    } 
+}
+*/
+
+describe("GoalModal", () => {
+    let mountedGoalModal;
+    let onSubmit = jest.fn();
+
+    //Creates a rendered GoalModal component to test
+    const goalModal = () => {
+        if(!mountedGoalModal) {
+            mountedGoalModal = mount(<GoalModal onSubmit={onSubmit}/>);
+        }
+    
+        return mountedGoalModal;
+    }
+    
+    //Resets the rendered GoalModal component before each test
+    beforeEach(() => {
+        mountedGoalModal = undefined;
+        onSubmit = jest.fn();
+    });
+
+    it("always renders a div", () => {
+        const divs = goalModal().find("div");
+        expect(divs.length).toBeGreaterThan(0);
+    });
+
+    describe("the rendered div", () => {
+        it("contains everything else that gets rendered", () => {
+            const divs = goalModal().find("div");
+            const wrappingDiv = divs.first();
+    
+            expect(wrappingDiv.children()).toEqual(goalModal().find("div").first().children());
+        });
+      });
+});
